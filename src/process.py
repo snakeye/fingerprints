9fe872ea8bc0467801193ae59da1f6d46f058064
Andrey Ovcharov <andrey.ovcharov@influencerdb.com>
2019-10-28 10:09:35 +0100

    .

--- src/process.py
+++ src/process.py
@@ -2,14 +2,33 @@ import os
 from PIL import Image
 
 
-def process(file):
-    print("Processing `{}`".format(file))
-    image = Image.open(file)
+def process_image(image):
+    new_image = Image.new('L', image.size)
 
     width, height = image.size
 
-    
+    for x in range(width):
+        for y in range(height):
+            px = image.getpixel((x,y))
+
+            new_image.putpixel((x, y), px)
+
+    return new_image
+
+
+def process(file):
+    # load image
+    image = Image.open(file)
+
+    # process image
+    new_image = process_image(image)
+
+    # save the result
+    ofilename = os.path.join('results', os.path.basename(file))
+    new_image.save(ofilename, 'PNG')
+
 
 for file in os.listdir("database"):
     if file.endswith(".png"):
+        print("Processing `{}`".format(file))
         process(os.path.join("database", file))
